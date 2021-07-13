725fe5f823c9bb5c15f32cea04fd0123e1813253
Claudio <claudio.leiva.r@gmail.com>
2021-07-13 18:37:32 -0400

    videojuego

--- ejercicio2_arreglos/ejercicio2_clase2.rb
+++ ejercicio2_arreglos/ejercicio2_clase2.rb
@@ -34,13 +34,18 @@ puts "7.- salir"
     elsif opcion == "3"
 
         puts "el uso de los signos +-*/ con un array de numeros en rubi:"
-        ar1 = [1,2,3,4].to_i
+        ar1 = [1,2,3,4]
         ar2 = [5,6,7,8]
         
         puts "suma del array1 + array2 = #{ar1+ar2}"
         puts "resta del array1 - arrya2 =  #{ar1-ar2}"
-        puts "multiplicacion del array1 * array2 =  #{ar1*5}"
-       # puts "division de 8 / 4 =  #{division}"
+        puts "multiplicacion del array1 * array2 es imposible"
+        puts "pero solo un array si se puede; arra1 * 3 =  #{ar1*3}"
+        puts "division de 8 / 4 es imposible en ruby"
+
+    elsif opcion ==4
+
+
     
     
     end    
---
+++ ejercicio2_arreglos/videojuego.rb
@@ -0,0 +1,21 @@
+vida = rand(50...20000)
+
+puts "#{vida} inicial"
+golpes = 0.to_i
+while vida !=0
+   punch = vida /2
+    if (punch.to_i % 2) != 0
+        puts "daño"       
+        vida = punch
+        puts vida 
+        golpes=golpes + 1
+    else
+        puts "daño"
+        punch-1
+        vida = punch
+       puts vida
+       golpes=golpes + 1
+    end
+    
+end
+puts "se necesitraron #{golpes} golpes para eliminar al monstruo"
