a101d9a47de0af9bea69b2567e302f36bb2086b5
Claudio <claudio.leiva.r@gmail.com>
2021-07-13 19:48:17 -0400

    arreglo ejercicio3

--- ejercicio2_arreglos/ejercicio3_clase2.rb
+++ ejercicio2_arreglos/ejercicio3_clase2.rb
@@ -1,13 +1,13 @@
 suma=0
- # promedio=0
+venta=0
 num = 0
 opcion=""
-while opcion != "1"
+while opcion != "-1"
     puts "ingrese el valor venta:"
-    suma=gets.chomp.to_i
-    suma =suma +suma
+    venta=gets.chomp.to_i
+    suma =suma + venta
     num= num+1
-    puts "para continuar presione enter, para salir escriba 1"
+    puts "para continuar presione enter, para salir escriba -1"
     opcion =gets.chomp
 end
 puts "ventas del dia #{suma}"
